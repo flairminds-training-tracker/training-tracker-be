f6b8c0a2caa5131b35e41d48b5a97220a70a0300
shivanivkulkarni <shivani.kulkarni@flairminds.com>
2023-10-20 19:05:45 +0530

    added get training activities api

--- models/activitiesModel.js
+++ models/activitiesModel.js
@@ -153,4 +153,37 @@ const saveTpModel = async(params, user_id) => {
     }
 };
 
-module.exports = { assignDueDateQuery , markActivitiesQuery , completionPercentage , getActivitiesByTechnologyQuery , setActivitiesRequiredQuery , saveTpModel}
+const getTrainingActModel= async(params) =>{
+    try {
+        const selectQuery = ` SELECT  
+        technologies_master.technology AS technology,
+        activities_master.activity AS activity_name,
+        tech_topics_master.topic AS topic,
+        tech_sub_topics_master.sub_topic AS sub_topic,
+        training_plan.start_date AS start_date,
+        training_plan.due_date AS due_date,
+        training_plan.end_date AS end_date,
+        comments.comment AS comments,
+        activities_master.resource_link AS resource_link,
+        activities_master.description AS description,
+        status_master.status AS status
+        FROM training_plan
+        INNER JOIN activities_master ON training_plan.activity_id = activities_master.activity_id
+        INNER JOIN tech_sub_topics_master ON activities_master.sub_topic_id = tech_sub_topics_master.tech_sub_topic_id
+        INNER JOIN tech_topics_master ON tech_sub_topics_master.tech_topic_id = tech_topics_master.tech_topic_id
+        INNER JOIN technologies_master ON tech_topics_master.tech_id = technologies_master.tech_id
+        LEFT JOIN comments ON training_plan.training_plan_id = comments.training_plan_id
+        INNER JOIN status_master ON training_plan.status_id = status_master.status_id
+        INNER JOIN trainee_trainer_tech ON training_plan.ttt_id = trainee_trainer_tech.ttt_id
+        INNER JOIN users ON trainee_trainer_tech.trainee_id = users.user_id
+        WHERE trainee_trainer_tech.trainee_id = ? `;
+        return executeQuery(selectQuery ,params);
+
+    } catch (error) {
+        console.log("Error in the activities model query");
+        throw error;
+    }
+
+}
+
+module.exports = { assignDueDateQuery , markActivitiesQuery , completionPercentage , getActivitiesByTechnologyQuery , setActivitiesRequiredQuery , saveTpModel, getTrainingActModel}
