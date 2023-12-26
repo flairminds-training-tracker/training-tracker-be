4fbe7976f374f5fb624813517a5c8d2e9c1b425e
punit-fm <106648664+punit-fm@users.noreply.github.com>
2023-12-26 12:16:21 +0530

    Update traineeController.js Added status dropdown API

--- controllers/traineeController.js
+++ controllers/traineeController.js
@@ -1,4 +1,4 @@
-const {getTrainee , getTraineeDetailsQuery, markAsReviewedQuery, getTraineesDetailsForStatusQuery, getStatusDropdownQuery , getStatusQuery} = require('../models/traineeModel.js');
+const {getTrainee , getTraineeDetailsQuery, markAsReviewedQuery, getTraineesDetailsForStatusQuery, getStatusDropdownQuery} = require('../models/traineeModel.js');
 const {executeQuery} = require('../db_config/db_schema.js')
 
 // 5 . Get Trainee & Trainer Dropdown - Admin Page
@@ -19,7 +19,7 @@ const markAsReviewedCtrl = async(req , res)=>{
     try {
         const results = markAsReviewedQuery(req.body.training_plan_id);
         if (!results.error) {
-            return res.send("Record updated..");
+            return res.send(results);
         }
         return res.send({error: true, errorMessage: results.errorMessage}) 
     } catch (error) {
@@ -28,15 +28,28 @@ const markAsReviewedCtrl = async(req , res)=>{
     }
 }
 // 14 . View Status for trainee - Training Page 
+const getTraineesDetailsForStatusCtrl = async(req, res)=>{
+    try {
+        const results = await getTraineesDetailsForStatusQuery(req.params);
+        if (!results.error) {
+            return res.send(results);
+        }
+        return res.send({error: true, errorMessage: error.message})
+    } catch (error) {
+        console.error("Error in view Status Ctrl :", error);
+        res.status(500).send("Internal Server Error"); 
+    }
+}
+// 11 . Trainee Details which are active or old - Trainees Page  
 const getActiveOrNotCtrl = async (req, res) => {
     try {
         const selectQuery = `SELECT * FROM users WHERE is_admin = 1 AND user_id = ? `;
         let user_id = req.user.user_id;
-        const adminExists = await executeQuery(selectQuery, user_id);
-        if (adminExists.length > 0) {
+        const adminExists =  await executeQuery(selectQuery, user_id);
+        if(adminExists.length > 0){
             user_id = null;
-        }
-        const results = await getTraineeDetailsQuery([req.query.activityType, user_id]);
+        }       
+        const results = await getTraineeDetailsQuery([req.query.activityType , user_id]);
         if (!results.error) {
             return res.send(results);
         }
@@ -60,18 +73,4 @@ const getStatusDropdownCtrl = async(req , res)=>{
         res.status(500).send("Internal Server Error");
     }
 }
-const getAllStatusCtrl = async(req , res)=>{
-    try {
-        const results = await getStatusQuery();
-        if (!results.error) {
-            return res.send(results);
-        }
-        return res.send({error: true, errorMessage: results.errorMessage})
-    } catch (error) {
-        console.error("Error in getAllStatusCtrl..:", error);
-        res.status(500).send("Internal Server Error");
-    }
-}
-
-getStatusQuery
-module.exports = {getTraineeCtrl  , getActiveOrNotCtrl , markAsReviewedCtrl  , getStatusDropdownCtrl ,  getActiveOrNotCtrl , getAllStatusCtrl};
+module.exports = {getTraineeCtrl  , getActiveOrNotCtrl , markAsReviewedCtrl , getTraineesDetailsForStatusCtrl , getStatusDropdownCtrl ,  getActiveOrNotCtrl};
